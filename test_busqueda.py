d777004a05f09ea7f0d6a109165fabdfacc6dce6
Fabián Hernández <fahg76@gmail.com>
2025-06-26 22:58:28 -0400

    commit 4

--- test_busqueda.py
+++ test_busqueda.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+
 
 
 
